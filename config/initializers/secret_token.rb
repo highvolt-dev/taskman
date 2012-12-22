65c4cd9467cffdcf286996e4c50b99503ea8e904
Jeremy Hough <highvoltage@gmail.com>
2012-12-21 22:38:12 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskman::Application.config.secret_token = '417d4855e92be087e031487e14fe471f97cc75feee537de854febb972cc298bfae0c97d8a6295fae89bb5cc092be4917e99a054c538d5e80291169ff7eca977e'
